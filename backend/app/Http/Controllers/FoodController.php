76acd65aa006f2be0ba7c01c0efcb51559f6f036
Ceejay Santos <7078ceejay@gmail.com>
2025-09-05 18:00:49 +0800

    Food Crud setup and Auth Setup

---
+++ backend/app/Http/Controllers/FoodController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Food;
+use App\Http\Requests\StoreFoodRequest;
+use App\Http\Requests\UpdateFoodRequest;
+
+class FoodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+         return Food::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFoodRequest $request)
+    {
+        $validated = $request->validate([
+            'thumbnail' => ['nullable', 'file', 'mimes:jpg,jpeg,png,pdf,gif', 'max:51200'], 
+            'food_name' => ['required', 'string', 'max:255'],
+            'price' => ['required', 'numeric', 'min:0'], 
+            'available' => ['required', 'boolean'], 
+            'description' => ['required', 'string', 'max:100'],
+        ]);
+
+        if ($request->hasFile('Thumbnail')) {
+            $file = $request->file('Thumbnail');
+            $filePath = $file->store('Thumbnails', 'public');
+            $validated['thumbnail'] = $filePath;
+        }
+
+        $created_food = Food::create($validated);
+        return $created_food;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Food $food)
+    {
+       return  $food;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFoodRequest $request, Food $food)
+    {
+        $validated = $request->validate([
+            'thumbnail' => ['nullable', 'file', 'mimes:jpg,jpeg,png,pdf,gif', 'max:51200'], 
+            'food_name' => ['required', 'string', 'max:255'],
+            'price' => ['required', 'numeric', 'min:0'], 
+            'available' => ['required', 'boolean'], 
+            'description' => ['required', 'string', 'max:100'],
+        ]);
+
+        if ($request->hasFile('thumbnail') && $request['thumbnail'] != null) {
+            $file = $request->file('thumbnail');
+            $filePath = $file->store('Thumbnails', 'public'); 
+            $validated['thumbnail'] = $filePath;
+        }else{
+            $validated['thumbnail'] = $food->thumbnail;
+        }
+
+        $food->update($validated);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Food $food)
+    {
+        $food->delete();
+
+        return ["message" => "post deleted"];
+    }
+}
