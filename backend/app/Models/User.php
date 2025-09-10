46297ebdf8cd80082c41ab29f4ae97b540f95eb0
Ceejay Santos <7078ceejay@gmail.com>
2025-09-10 23:53:49 +0800

    added order function in user

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -52,4 +52,8 @@ class User extends Authenticatable
     {
         return $this->hasMany(CartItem::class);
     }
+    public function Orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
