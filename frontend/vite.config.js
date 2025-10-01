b934e7bb5fe3cb900dfc74cce3c6da102b1fd9cf
Ceejay Santos <7078ceejay@gmail.com>
2025-10-01 13:20:28 +0800

    network configs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  server:{ allowedHosts: ['fronten.loca.lt'],},
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  // load variables from .env files
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    server: {
+      allowedHosts: [env.VITE_HOST_URL], // 👈 use env here
+      host: '0.0.0.0',
+    },
+    plugins: [react(), tailwindcss()],
+  }
 })
