4c06442b204188d4cfe60bd1e8843f3eee1f8eba
Ceejay Santos <7078ceejay@gmail.com>
2025-10-23 18:37:45 +0800

    fix saving user update

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\HandleCors;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\JsonResponse;
 
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -19,4 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
+        $exceptions->render(function (AuthenticationException $e, $request) {
+            if ($request->expectsJson()) {
+                return new JsonResponse(['message' => 'Unauthenticated.'], 401);
+            }
+        });
     })->create();
