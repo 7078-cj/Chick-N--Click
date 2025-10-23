91bccdde519900a2a31313c9373d6d80f7b3fb61
Ceejay Santos <7078ceejay@gmail.com>
2025-10-23 18:46:30 +0800

    fix#2

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('/register',[AuthController::class, 'register']);
-Route::post('/login',[AuthController::class, 'login']);
+Route::post('/login',[AuthController::class, 'login'])->name('login');
 Route::get('/user',[AuthController::class, 'userDetails'])->middleware('auth:sanctum');
 Route::put('/user/update',[AuthController::class, 'updateUser'])->middleware('auth:sanctum');
 Route::post('/logout',[AuthController::class, 'logout'])->middleware('auth:sanctum');
